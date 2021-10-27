2568acf1af83d42e3d1799646cda02d012bb6512
BouchetaYoucef <87813331+BouchetaYoucef@users.noreply.github.com>
2021-10-27 11:30:04 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -3,7 +3,13 @@ import streamlit as st
 import pandas as pd
 import streamlit as s
 import matplotlib.pyplot as plt
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title("Hello world!")
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None
+  df = pd.read_csv(uploaded_file, sep="|")
+  st.write(df)
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None

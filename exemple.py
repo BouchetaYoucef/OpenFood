8ecbd229e7d1b205400226dac41fcb3fc7ea032b
BouchetaYoucef <87813331+BouchetaYoucef@users.noreply.github.com>
2021-10-27 11:26:58 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -6,7 +6,13 @@ import matplotlib.pyplot as plt
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Hello world!")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title("Hello world!")
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None
+  df = pd.read_csv(uploaded_file, sep="|")
+  st.write(df)
 uploaded_file = st.file_uploader("nutrition_app")
 if uploaded_file is not None
   df = pd.read_csv(uploaded_file, sep="|")
